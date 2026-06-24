56a30024925877f428c12321001d86f5ea40c72b
Arthur-codeX <daniel.karanja239@gmail.com>
2026-06-24 15:41:53 +0300

    updated docs

--- JS/Intro/ifOnly.js
+++ JS/Intro/ifOnly.js
@@ -18,7 +18,7 @@ lets start with if .
 
 //->free to enter any age
 //->Hope -> bible ->
-//->POut number 
+//->POut number
 
 /*
  bugs ->
@@ -27,37 +27,42 @@ lets start with if .
  
 */
 
-
-let age=prompt("Enter your age:")
+let age = prompt("Enter your age:");
 //
-console.log(`age is ${age} typeof ${typeof age}`)
-alert(`You entered ${age}`)
+console.log(`age is ${age} typeof ${typeof age}`);
+alert(`You entered ${age}`);
 //NaN
 //+age ->syntatical
 //isNaN, Number , +
-   // age=15
-    //15>10:true 
+// age=15
+//15>10:true
 
-    if(age>10){
-        alert('You are a baby.')
-    }
-    //15>13=true
-    if(age>13){
-        alert(`You are a teen`)
-    }
-    //15>18=false
-    if(age>18){
-        alert("You can legally drink and smoke")
-    }
-    //27>27=false
-    if(age>27){
-        alert("Best age for marriage")
-    }
-    //27>45=false
-    if(age>45){
-        alert("Best age for retiremnt")
-    }
-    //27>50=false
-    if(age>50){
-        alert("You are an ansestor")
-    }
+/*
+ Work on this bit->
+ using if statments only to ensure we get only one alert for age.
+ must be correct one.
+*/
+
+if (age > 10) {
+  alert("You are a baby.");
+}
+//15>13=true
+if (age > 13) {
+  alert(`You are a teen`);
+}
+//15>18=false
+if (age > 18) {
+  alert("You can legally drink and smoke");
+}
+//27>27=false
+if (age > 27) {
+  alert("Best age for marriage");
+}
+//27>45=false
+if (age > 45) {
+  alert("Best age for retiremnt");
+}
+//27>50=false
+if (age > 50) {
+  alert("You are an ansestor");
+}
