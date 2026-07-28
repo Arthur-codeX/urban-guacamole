dcddda3a6a507972bc5f5dece1ed4a677e0ee82e
Arthur-codeX <daniel.karanja239@gmail.com>
2026-07-28 15:17:03 +0300

    started ion the navigation section.

---
+++ REACT/src/AmazonPage/Navigation.jsx
@@ -0,0 +1,26 @@
+/*
+ top navigation of the page
+
+*/
+/*
+import <name> from "<relative path>"
+ensure before import its in the src directory
+*/
+import logo from "./assets/logo.png";
+
+/* component naming rules.*/
+/*null or valid jsx*/
+function Navigation() {
+  return (
+    <div>
+      <h2>Navigation section</h2>
+    </div>
+  );
+}
+
+/*
+ export so that any othe file can
+ import the component.
+ 1.Default Export:<one major>
+*/
+export default Navigation;
