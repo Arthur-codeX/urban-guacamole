4d095530dcb9b781538c4592c89d27b4d1da567d
ArthurcodeX <daniel.karanja239@gmail.com>
2026-08-26 15:22:16 +0300

    started kwargs

--- PYTHON/Intro/function.py
+++ PYTHON/Intro/function.py
@@ -5,13 +5,37 @@
 
 #def-> pi
 
+"""
+Multi line comments 
+Multi line comments 
+Multi line comments 
+"""
+
+'''
+Multi line comments 
+Multi line comments 
+Multi line comments 
+'''
+
 def area_cirlce(radius):
     ans= 3.142*pow(radius,2)
-    print(f"for circle of radius {radius} area is {ans}")
-    return
+    #print(f"for circle of radius {radius} area is {ans}")
+    return ans
 
-area_cirlce(20)
+def print_area_circle(radius):
+    ans=area_cirlce(radius)
+    
 
+radius=int(input("Enter radiues"))
+area_cirlce(radius)
+
+#strongly typed 
 new_f=area_cirlce
 
-new_f(10)
+#Strongly typed languages
+#String name="Richie"
+#name=23
+#rust -> c++
+# 
+radius=int(input("Enter radiues"))
+new_f(radius)
